9d7d657aa211c5a9753d461ffefe9b4f9ac27d75
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-11-05 20:59:07 -0300

    Criação da página de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
 ];
 
 @NgModule({
